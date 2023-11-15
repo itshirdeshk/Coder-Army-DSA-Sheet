4f4477ce02b7f94963f919ef9e5d67025020dc3e
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-11-15 10:11:09 +0530

    Day 5

--- Day 5/code.cpp
+++ Day 5/code.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
 // Q1. Find Transition Point.
@@ -64,6 +64,99 @@ void rearrange(int arr[], int n)
             else if (y < neg.size())
                 arr[i] = neg[y++];
         }
+    }
+}
+
+// Q3. Union of Two Sorted Arrays.
+vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+{
+    // Your code here
+    // return vector with correct order of elements
+    int i = 0, j = 0;
+    vector<int> ans;
+    while (i < n && j < m)
+    {
+        if (arr1[i] == arr2[j])
+        {
+            if (arr1[i] == arr1[i - 1])
+            {
+                i++, j++;
+            }
+            else
+            {
+                ans.push_back(arr1[i]);
+                i++, j++;
+            }
+        }
+        else if (arr1[i] < arr2[j])
+        {
+            if (i == 0)
+            {
+                ans.push_back(arr1[i++]);
+            }
+            else if (arr1[i] == arr1[i - 1])
+            {
+                i++;
+            }
+            else
+            {
+                ans.push_back(arr1[i++]);
+            }
+        }
+        else if (arr1[i] > arr2[j])
+        {
+            if (j == 0)
+            {
+                ans.push_back(arr2[j++]);
+            }
+            else if (arr2[j] == arr2[j - 1])
+            {
+                j++;
+            }
+            else
+            {
+                ans.push_back(arr2[j++]);
+            }
+        }
+    }
+
+    if (i < n)
+    {
+        for (int x = i; x < n; x++)
+            if (arr1[x] == arr1[x - 1])
+            {
+                continue;
+            }
+            else
+                ans.push_back(arr1[x]);
+    }
+    else
+    {
+        for (int x = j; x < m; x++)
+            if (arr2[x] == arr2[x - 1])
+            {
+                continue;
+            }
+            else
+                ans.push_back(arr2[x]);
+    }
+
+    return ans;
+}
+
+// Q4. Three way partitioning.
+void threeWayPartition(vector<int> &array, int a, int b)
+{
+    // code here
+    int i = 0, j = 0, k = array.size() - 1;
+    while (j <= k)
+    {
+        if (array[j] < a)
+            swap(array[i++], array[j++]);
+        else if (array[j] >= a && array[j] <= b)
+            j++;
+        else
+            swap(array[j], array[k--]);
     }
 }
 
