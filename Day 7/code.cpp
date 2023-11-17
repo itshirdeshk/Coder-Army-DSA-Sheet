dd86d75030b7bc8615cc9df33e5d1b37477fb9ce
Hirdesh Khandelwal <hirdeshkhandelwal58@gmail.com>
2023-11-17 13:32:19 +0530

    Day 7

--- Day 7/code.cpp
+++ Day 7/code.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 
 // Q1. Rearrange Array Alternately.
@@ -27,6 +28,50 @@ void rearrange(long long *arr, int n)
         arr[i] /= maxi;
 }
 
-// Q2. 
+// Q2. Pythagorean Triplet.
+
+// Q3. Rearrange an array with O(1) extra space.
+void arrange(long long arr[], int n)
+{
+    // Your code here
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] += (arr[arr[i]] % n) * n;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] /= n;
+    }
+}
+
+// Q4. Count triplets with sum smaller than X.
+long long countTriplets(long long arr[], int n, long long sum)
+{
+    // Your code goes here
+    sort(arr, arr + n);
+    long long count = 0;
+    for (int k = 0; k < n - 2; k++)
+    {
+        int i = k + 1;
+        int j = n - 1;
+        while (j > i)
+        {
+            long long x = arr[i] + arr[j] + arr[k];
+            if (x < sum)
+            {
+                count += (j - i);
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+    }
+    return count;
+}
+
+// Q5. 
 
 int main() {}
